d672f3ea4cecc34c85f2db385bf9bf7692816567
hamza kılıç <hamza2358@hotmail.com>
2023-10-28 14:19:39 +0300

    added couponCreate and couponDelete

--- FrontEnd/Web/Controllers/CouponController.cs
+++ FrontEnd/Web/Controllers/CouponController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using Web.Models;
 using Web.Service.IService;
 
@@ -8,8 +9,8 @@ namespace Web.Controllers
     public class CouponController : Controller
     {
         private readonly ICouponService _couponService;
-        public CouponController(ICouponService couponService) 
-        { 
+        public CouponController(ICouponService couponService)
+        {
             _couponService = couponService;
         }
         public async Task<IActionResult> CouponIndex()
@@ -17,12 +18,44 @@ namespace Web.Controllers
             List<CouponDto>? list = new();
             ResponseDto? response = await _couponService.GetAllCouponsAsync();
 
-            if(response !=null && response.IsSuccess)
+            if (response != null && response.IsSuccess)
             {
                 list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
             }
 
             return View(list);
+        }
+
+        public async Task<IActionResult> CouponCreate()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CouponCreate(CouponDto couponDto)
+        {
+            if (ModelState.IsValid)
+            {
+                ResponseDto? response = await _couponService.CreateCouponAsync(couponDto);
+
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(CouponIndex));
+                }
+            }
+
+            return View(couponDto);
+        }
+
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            ResponseDto? response = await _couponService.DeleteCouponAsync(couponId);
+
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(CouponIndex));
+            }
+
+            return NotFound();
         }
     }
 }
